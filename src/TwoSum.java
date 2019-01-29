1ba4eace99dc80b0ee54354b608b43432d02a84d
叶志鹏 <geekye.ye@ly.com>
2019-01-29 11:22:29 +0800

    commit clever way to get index of twoSum question

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 4, 5};
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
         int target = 8;
-        int[] indexes = simpleWay(nums, target);
+        int[] indexes = cleverWay(nums, target);
+        Arrays.sort(indexes);
         StringBuilder result = new StringBuilder();
         result.append("Array‘s indexes are ");
         for (int i = 0; i < indexes.length; i++) {
@@ -27,6 +32,25 @@ public class TwoSum {
                 }
             }
         }
+        return indexes;
+    }
+
+    private static int[] cleverWay(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        int[] indexes = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (Integer key : map.keySet()
+                ) {
+            if (map.get(target - map.get(key)) != null) {
+                indexes[0] = map.get(key);
+                indexes[1] = map.get(target - key);
+            }
+        }
+
         return indexes;
     }
 }
